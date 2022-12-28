f505604efa4021828c6f72b417d4f770c794f012
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-12-28 15:25:02 +0100

    Add filter by region

--- src/components/Main/FilterMenu/FilterMenu.js
+++ src/components/Main/FilterMenu/FilterMenu.js
@@ -1,5 +1,22 @@
 import "./FilterMenu.css";
 
-const FilterMenu = () => {};
+const FilterMenu = ({ data, setSelectedRegion }) => {
+  const regions = [...new Set(data.map((country) => country.region))].sort();
+
+  return (
+    <div className="dropdown">
+      <button className="dropbtn">Dropdown</button>
+      <div className="dropdown-content">
+        {regions?.map((region) => {
+          return (
+            <p key={region} onClick={() => setSelectedRegion(region)}>
+              {region}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default FilterMenu;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,8 @@
 import "./Main.css";
 import classnames from "classnames";
+import axios from "axios";
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import GlobalContext from "../../context/GlobalContext";
 import { useAsyncValue } from "react-router-dom";
 
@@ -12,19 +13,41 @@ import CountriesSection from "./CountriesSection/CountriesSection";
 const Main = () => {
   const { isDarkModeOn } = useContext(GlobalContext);
   const { data } = useAsyncValue();
-  const [searchInput, setSearchInput] = useState("");
 
-  let showedCards = data.filter((country) =>
-    country.name.common.toLowerCase().includes(searchInput)
-  );
+  const [searchInput, setSearchInput] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("");
+
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    if (selectedRegion) {
+      const regionFilterData = (filteredData) => {
+        setFilteredData(filteredData);
+      };
+
+      axios
+        .get(`https://restcountries.com/v3.1/region/${selectedRegion}`)
+        .then((res) => regionFilterData(res.data));
+    }
+  }, [selectedRegion]);
+
+  const searchFilter = (data) => {
+    return data.filter((country) =>
+      country.name.common.toLowerCase().includes(searchInput)
+    );
+  };
 
   return (
     <main className={classnames({ dark_mode_bg: isDarkModeOn })}>
       <div>
         <SearchBar setSearchInput={setSearchInput} />
-        <FilterMenu />
+        <FilterMenu data={data} setSelectedRegion={setSelectedRegion} />
       </div>
-      <CountriesSection data={showedCards} />
+      <CountriesSection
+        data={
+          filteredData.length ? searchFilter(filteredData) : searchFilter(data)
+        }
+      />
     </main>
   );
 };
